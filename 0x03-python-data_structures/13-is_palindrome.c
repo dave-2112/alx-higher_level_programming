38bde00df5ed082a6c917f044bdbfe6a88d452c0
dave-2112 <davemesfin16@gmail.com>
2022-08-28 21:20:09 +0300

    data structure

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,31 +1,53 @@
 #include "lists.h"
-
+#include <stdlib.h>
 /**
- * is_palindrome - function to call check_pal to see if list is palindrome
- * @head: ptr to the beginning of the list
- * Return: 0 if not palindrome else 1
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: is a parameter
+ * Return: if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	if (head == NULL || *head == NULL)
-		return (1);
-	return (check_pal(head, *head));
-}
+	listint_t *current = *head;
+	int len, j, result, *array;
 
-/**
- * check_pal - function to check if the list is palindrome
- * @head: ptr to the beginning of the list
- * @last: ptr to the end of the list
- * Return: 0 if not palindrom else 1
- */
-int check_pal(listint_t **head, listint_t *last)
-{
-	if (last == NULL)
+	if (*head == NULL)
 		return (1);
-	if (check_pal(head, last->next) && (*head)->n == last->n)
+	for (len = 0; current != NULL; len++)
+		current = current->next;
+	current = *head;
+	array = malloc(sizeof(int) * len);
+	if (array == NULL)
+		return (EXIT_FAILURE);
+	for (j = 0; current != NULL; j++)
 	{
-		*head = (*head)->next;
-		return (1);
+		array[j] = current->n;
+		current = current->next;
 	}
-	return (0);
+	result = check_palindrome(array, len);
+	return (result);
+}
+/**
+ * check_palindrome - is a palindrome.
+ * @array: is a parameter
+ * @len: is a length list
+ * Return: if it is not a palindrome, 1 if it is a palindrome
+ */
+int check_palindrome(int *array, int len)
+{
+	int i = 0, j = len - 1;
+
+	for (i = 0; i < j; i++, j--)
+	{
+		if (array[i] == array[j])
+			continue;
+			else if (array[i] != array[j])
+            break;
+    }
+    if (array[i] != array[j])
+    {
+        free(array);
+        return (0);
+    }
+    free(array);
+    return (1);
 }
