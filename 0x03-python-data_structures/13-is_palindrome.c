5eab00455baae6a97910edcdc240fb92da2af72e
dave-2112 <davemesfin16@gmail.com>
2022-08-28 21:26:45 +0300

    data structure

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,53 +1,46 @@
+
 #include "lists.h"
-#include <stdlib.h>
+
 /**
  * is_palindrome - checks if a singly linked list is a palindrome.
- * @head: is a parameter
- * Return: if it is not a palindrome, 1 if it is a palindrome
+ * @head: Double pointer to listint_t single linked list's head
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	int len, j, result, *array;
+	listint_t *tmp_beg, *tmp_end;
+	int counter1, counter2, c1, c2;
 
-	if (*head == NULL)
+	if (!*head)
 		return (1);
-	for (len = 0; current != NULL; len++)
-		current = current->next;
-	current = *head;
-	array = malloc(sizeof(int) * len);
-	if (array == NULL)
-		return (EXIT_FAILURE);
-	for (j = 0; current != NULL; j++)
+	tmp_beg = *head;
+	tmp_end = *head;
+	counter1 = 0, counter2 = 0;
+	while (tmp_end->next)
 	{
-		array[j] = current->n;
-		current = current->next;
+		counter2++;
+		tmp_end = tmp_end->next;
 	}
-	result = check_palindrome(array, len);
-	return (result);
-}
-/**
- * check_palindrome - is a palindrome.
- * @array: is a parameter
- * @len: is a length list
- * Return: if it is not a palindrome, 1 if it is a palindrome
- */
-int check_palindrome(int *array, int len)
-{
-	int i = 0, j = len - 1;
-
-	for (i = 0; i < j; i++, j--)
+	tmp_end = *head;
+	while (counter1 < counter2)
 	{
-		if (array[i] == array[j])
-			continue;
-			else if (array[i] != array[j])
-            break;
-    }
-    if (array[i] != array[j])
-    {
-        free(array);
-        return (0);
-    }
-    free(array);
-    return (1);
+		c1 = 0, c2 = 0;
+		while (c1 < counter1)
+		{
+			tmp_beg = tmp_beg->next;
+			c1++;
+		}
+		while (c2 < counter2)
+		{
+			tmp_end = tmp_end->next;
+			c2++;
+		}
+		if (tmp_beg->n != tmp_end->n)
+			return (0);
+		counter1++;
+		counter2--;
+		tmp_beg = *head;
+		tmp_end = *head;
+	}
+	return (1);
 }
