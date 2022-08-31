743fcaa4421071387d4563ffff38cba46be78fd1
dave-2112 <davemesfin16@gmail.com>
2022-09-01 00:08:01 +0300

    more data structure

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    res_list = []
-    res = 0
-    for item in my_list:
-        if item not in res_list:
-            res_list.append(item)
-    for uniqs in res_list:
-        res += uniqs
-    return res
-    
+def common_elements(set_1, set_2):
+    return set(set_1) & set(set_2)
