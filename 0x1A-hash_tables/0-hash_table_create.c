9547bfb41d4c41fdac74e2be6718b03934c95af0
dave-2112 <davemesfin16@gmail.com>
2022-09-22 23:59:20 +0300

    Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,31 +1,66 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_create - function that creates a hash table.
- * @size: size of the hash table.
- * Return: a pointer to the newly created hash table otherwhise returns NULL
+ * free_node - Free a node.
+ * @node: Node to free.
+ *
+ * Return: Void.
  */
-hash_table_t *hash_table_create(unsigned long int size)
+void free_node(hash_node_t *node)
 {
-	hash_table_t *new_hash_table = NULL;
+	free(node->key);
+	free(node->value);
+	free(node);
+}
 
-	new_hash_table = calloc(1, sizeof(hash_table_t));
-	if (new_hash_table == NULL)
-		return (NULL);
+/**
+ * hash_table_set - Set a value in the hash table.
+ * @ht: Hash table.
+ * @key: Key to be indexed.
+ * @value: Value to set in the hash table.
+ *
+ * Return: 1 if works, 0 if doesn't.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *current;
 
-	if (size == 0 || size == '\0')
+	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup((char *)key);
+	new_node->value = strdup((char *)value);
+	new_node->next = NULL;
+	if (ht->array[index] == NULL)
+		ht->array[index] = new_node;
+	else
 	{
-		free(new_hash_table);
-		return (NULL);
+		current = ht->array[index];
+		if (strcmp(current->key, key) == 0)
+		{
+			new_node->next = current->next;
+			ht->array[index] = new_node;
+			free_node(current);
+			return (1);
+		}
+		while (current->next != NULL && strcmp(current->next->key, key) != 0)
+		{ current = current->next;
+		}
+		if (strcmp(current->key, key) == 0)
+		{
+			new_node->next = current->next->next;
+			free_node(current->next);
+			current->next = new_node;
+		}
+		else
+		{
+			new_node->next = ht->array[index];
+			ht->array[index] = new_node;
+		}
 	}
-
-	new_hash_table->size = size;
-
-	new_hash_table->array = calloc(size, sizeof(hash_node_t *));
-	if (new_hash_table->array == NULL)
-	{
-		free(new_hash_table);
-		return (NULL);
-	}
-
-	return (new_hash_table);
+	return (1);
 }
