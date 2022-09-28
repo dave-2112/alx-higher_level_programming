5312071908bd746776bb4e9bfc7b27b2cbb0b245
dave-2112 <davemesfin16@gmail.com>
2022-09-28 18:41:18 +0300

    python-input_output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-""" Module for student class """
+"""define Python class for JSON serialization of an object
+"""
 
 
-class Student():
-    """ Student class """
-    def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self):
-        return self.__dict__
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure
+    """
+    return obj.__dict__
